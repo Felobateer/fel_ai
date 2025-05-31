38334355b96cd7eba4ace68e0937ec95278ccb9e
Felobateer <felobateersamy01@gmail.com>
2025-05-31 22:34:07 +0200

    add engine

--- engine.py
+++ engine.py
@@ -34,3 +34,73 @@ def train_step(
     train_acc = train_acc / len(dataloader)
     return train_loss, train_acc
 
+def test_step(
+        model: torch.nn.Module,
+        dataloader: torch.utils.data.DataLoader,
+        loss_fn: torch.nn.Module,
+        device: torch.device
+) -> Tuple[float, float]:
+    model.eval()
+
+    test_loss, test_acc = 0, 0
+
+    with torch.inference_mode():
+        for batch, (X, y) in enumerate(dataloader):
+            X, y = X.to(device), y.to(device)
+
+            test_prediction = model(X)
+
+            loss = loss_fn(test_prediction, y)
+            test_loss += loss.item()
+
+            test_pred_label = test_prediction.argmax(dim=1)
+            test_acc += ((test_pred_label == y).sum().item()/len(test_pred_label))
+    test_loss = test_loss / len(dataloader)
+    test_acc = test_acc / len(dataloader)
+    return test_loss, test_acc
+
+def train(
+        model: torch.nn.Module,
+        train_dataloader: torch.utils.data.DataLoader,
+        test_dataloader: torch.utils.data.DataLoader,
+        optimizer: torch.optim.Optimizer,
+        loss_fn: torch.nn.Module,
+        epochs: int,
+        device: torch.device
+) -> Dict[str, List]:
+    results = {
+        "train_loss": [],
+        "train_acc": [],
+        "test_loss": [],
+        "test_acc": []
+    }
+
+    model.to(device)
+
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(
+            model=model,
+            dataloader=train_dataloader, 
+            loss_fn=loss_fn,
+            optimizer=optimizer,
+            device=device
+        )
+        test_loss, test_acc = test_step(
+            model=model,
+            dataloader=test_dataloader,
+            loss_fn=loss_fn,
+            device=device
+        )
+        print(
+            f"Epoch: {epoch+1} | "
+            f"train_loss: {train_loss:.4f} |"
+            f"train_acc: {train_acc:.4f} |"
+            f"test_loss: {test_loss:.4f} |"
+            f"test_acc: {test_acc:.4f} |"
+        )
+        results['train_loss'].append(train_loss)
+        results['train_acc'].append(train_acc)
+        results['test_loss'].append(test_loss)
+        results['test_acc'].append(test_acc)
+    
+    return results
