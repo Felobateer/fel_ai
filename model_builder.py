f53cf17f44d6001ec755a5e0efc64f5e9f2e2c60
Felobateer <felobateersamy01@gmail.com>
2025-06-08 23:42:22 +0200

    fix model_builder

--- model_builder.py
+++ model_builder.py
@@ -5,16 +5,16 @@ class TinyVGG(nn.Module):
     def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
         super().__init__()
         self.conv_block_1 = nn.Sequential(
-            nn.conv2d(in_channels = input_shape, out_channels=hidden_units, kernel_size=3, stride=1, padding=0),
+            nn.Conv2d(in_channels = input_shape, out_channels=hidden_units, kernel_size=3, stride=1, padding=0),
             nn.ReLU(),
-            nn.conv2d(in_channels = hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=0),
+            nn.Conv2d(in_channels = hidden_units, out_channels=hidden_units, kernel_size=3, stride=1, padding=0),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
         self.conv_block_2 = nn.Sequential(
-            nn.conv2d(hidden_units, hidden_units, kernel_size=3, padding=0),
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=0),
             nn.ReLU(),
-            nn.conv2d(hidden_units, hidden_units, kernel_size=3, padding=0),
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=0),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
